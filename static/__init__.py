99c40ec93ce7d22eca1f0a90315d49c2f60fd41a
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-01-07 09:47:46 +0100

    Nuke existing implementation

--- static/__init__.py
+++
@@ -1,6 +0,0 @@
-from .coordinators import make_single, make_multiple
-from .util import ConversionOpts, setup_logging
-
-"""
-`static` is a Python module responsible for creating a static GTFS feed.
-"""
