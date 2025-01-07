e4e03dd4915d10985e955202b5df1064dd888427
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-01-07 16:23:42 +0100

    Load routes from JSON

--- warsaw_gtfs/load_json/task.py
+++ warsaw_gtfs/load_json/task.py
@@ -2,8 +2,9 @@ from collections.abc import Iterable
 from typing import Any
 
 from impuls import DBConnection, Task, TaskRuntime
-from impuls.model import Stop
+from impuls.model import Route, Stop
 
+from .routes import parse_routes
 from .stops import parse_stops
 
 
@@ -21,19 +22,23 @@ class LoadJSON(Task):
         self.load_schedules(r)
 
     def load_lookup_tables(self, r: TaskRuntime) -> None:
+        self.logger.info("Loading lookup tables")
         data = r.resources["slowniki.json"].json()
         with r.db.transaction():
             self.load_stops(r.db, data)
-        self.load_routes(r.db, data["linie"], data["typy_linii"])
+            self.load_routes(r.db, data)
         self.load_calendars(r.db, data["kalendarz"], data["typy_dni"])
 
     def load_stops(self, db: DBConnection, data: Any) -> None:
+        self.logger.debug("Loading stops")
         db.create_many(Stop, parse_stops(data))
 
-    def load_routes(self, db: DBConnection, routes: Any, route_types: Any) -> None:
-        raise NotImplementedError
+    def load_routes(self, db: DBConnection, data: Any) -> None:
+        self.logger.debug("Loading routes")
+        db.create_many(Route, parse_routes(data))
 
     def load_calendars(self, db: DBConnection, days: Any, day_types: Any) -> None:
+        self.logger.debug("Loading calendars")
         raise NotImplementedError
 
     def load_schedules(self, r: TaskRuntime) -> None:
