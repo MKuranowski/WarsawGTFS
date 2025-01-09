04bee1481c6a8f43a0d67187e8b83bd347a51f6c
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-01-09 01:04:13 +0100

    Load variant_id from JSON

--- warsaw_gtfs/load_json/stop_times.py
+++ warsaw_gtfs/load_json/stop_times.py
@@ -3,6 +3,8 @@ from typing import Any
 
 from impuls.model import StopTime, TimePoint
 
+from ..util import compact_json
+
 
 def parse_stop_times(data: Any) -> Iterable[StopTime]:
     return map(parse_stop_time, data["kursy_przejazdy"])
@@ -15,4 +17,5 @@ def parse_stop_time(data: Any) -> StopTime:
         stop_sequence=data["numer_slupka"],
         arrival_time=TimePoint.from_str(data["p24"]),
         departure_time=TimePoint.from_str(data["o24"]),
+        extra_fields_json=compact_json({"variant_id": str(data["id_wariantu"])}),
     )
