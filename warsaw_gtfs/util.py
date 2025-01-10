eaf212ac905ac18286c06f92724354a0281f1947
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-01-10 19:38:15 +0100

    Merge virtual stops

--- warsaw_gtfs/util.py
+++ warsaw_gtfs/util.py
@@ -4,3 +4,7 @@ from typing import Any
 
 def compact_json(data: Any) -> str:
     return json.dumps(data, indent=None, separators=(",", ":"))
+
+
+def is_railway_stop(code: str) -> bool:
+    return code[1:3] in {"90", "91", "92"} or code == "1930"
