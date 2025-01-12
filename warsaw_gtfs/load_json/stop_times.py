0089454ded76d63f40ec26a3431663b1f3130b23
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-01-12 19:38:09 +0100

    Move id stabilization to load_json for performance

--- warsaw_gtfs/load_json/stop_times.py
+++ warsaw_gtfs/load_json/stop_times.py
@@ -1,4 +1,4 @@
-from collections.abc import Iterable
+from collections.abc import Iterable, Mapping
 from typing import Any
 
 from impuls.model import StopTime, TimePoint
@@ -6,14 +6,23 @@ from impuls.model import StopTime, TimePoint
 from ..util import compact_json
 
 
-def parse_stop_times(data: Any) -> Iterable[StopTime]:
-    return map(parse_stop_time, data["kursy_przejazdy"])
+def parse_stop_times(
+    data: Any,
+    trip_id_lookup: Mapping[int, str],
+    stop_id_lookup: Mapping[int, str],
+) -> Iterable[StopTime]:
+    for stop_time in data["kursy_przejazdy"]:
+        yield parse_stop_time(
+            stop_time,
+            trip_id_lookup[stop_time["id_kursu"]],
+            stop_id_lookup[stop_time["id_slupka"]],
+        )
 
 
-def parse_stop_time(data: Any) -> StopTime:
+def parse_stop_time(data: Any, trip_id: str, stop_id: str) -> StopTime:
     return StopTime(
-        trip_id=str(data["id_kursu"]),
-        stop_id=str(data["id_slupka"]),
+        trip_id=trip_id,
+        stop_id=stop_id,
         stop_sequence=data["numer_slupka"],
         arrival_time=TimePoint.from_str(data["p24"]),
         departure_time=TimePoint.from_str(data["o24"]),
