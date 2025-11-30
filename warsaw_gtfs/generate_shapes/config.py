4d98cc455ede101ddaad0c5f27696103d90b720b
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-11-30 15:40:54 +0100

    Shapes: separate helper objects

--- warsaw_gtfs/generate_shapes/config.py
+++ warsaw_gtfs/generate_shapes/config.py
@@ -4,25 +4,66 @@ import routx
 from impuls import selector
 from impuls.tools.types import StrPath
 
+from .model import BBox
+
 
 @dataclass
 class GraphConfig:
+    """Configures *how* the GenerateShapes task generates shapes."""
+
     osm_resource: str
+    """Name of the resource containing OSM data with the routing graph."""
+
     profile: routx.OsmProfile | routx.OsmCustomProfile
-    bbox: tuple[float, float, float, float] = (0.0, 0.0, 0.0, 0.0)
+    """Routx profile for parsing the OSM data."""
+
+    bbox: BBox = (0.0, 0.0, 0.0, 0.0)
+    """BBox for trimming the OSM data. Useful when osm_resource is a geofabrik data dump,
+    in order to reduce memory usage.
+
+    Defaults to (0, 0, 0, 0); which disables the bounding box.
+    """
+
     curation_resource: str = ""
+    """Name of a resource containing manual overrides for shape generation"""
+
     max_stop_to_node_distance: float = 100.0
+    """Max distance between a stop and its matched node in the graph."""
 
 
 @dataclass
 class GenerateConfig:
+    """Configures *what* the GenerateShapes task generates."""
+
     routes: selector.Routes
+    """Filter for routes for which the task generates shapes."""
+
     shape_id_prefix: str = ""
+    """Prefix for the assigned, numeric shape_ids.
+    Otherwise, the generated shapes will have ids as consequential numbers; which might
+    conflict with whatever's in the database.
+    """
+
     overwrite: bool = False
+    """Overwrite any existing shapes.
+
+    Defaults to False, where any selected trips with shapes will be skipped.
+    """
 
 
 @dataclass
 class LoggingConfig:
+    """Configures how the GenerateShapes task communicates errors."""
+
     task_name: str = "GenerateShapes"
+    """Name of the task, used for logging."""
+
     dump_errors: StrPath | None = None
+    """Directory for dumping shape generation failures in GeoJSON format.
+
+    Helps in debugging why certain stop pairs were connected by a straight line,
+    instead of an actual shape.
+    """
+
     clean_error_dir: bool = True
+    """Whether the directory selected by `dump_errors` should be cleaned before running."""
---
+++ warsaw_gtfs/generate_shapes/model.py
@@ -0,0 +1,74 @@
+from collections.abc import Iterable, Mapping, Sequence
+from dataclasses import dataclass, field
+from math import inf, nan
+from typing import NamedTuple, Self, TypeAlias
+
+import routx
+
+BBox: TypeAlias = tuple[float, float, float, float]
+LatLon: TypeAlias = tuple[float, float]
+
+StopIdSequence: TypeAlias = tuple[str, int]
+StopPair: TypeAlias = tuple[str, str]
+
+TripStops: TypeAlias = tuple[StopIdSequence, ...]
+TripIds: TypeAlias = Sequence[str]
+TripsByStops: TypeAlias = Mapping[TripStops, TripIds]
+
+ForceVia: TypeAlias = Mapping[StopPair, LatLon]
+RatioOverrides: TypeAlias = Mapping[StopPair, float]
+
+
+class LatLonDist(NamedTuple):
+    lat: float
+    lon: float
+    distance: float = nan
+
+    def with_distance_offset(self, offset: float) -> Self:
+        new_dist = round(self.distance + offset, 6)
+        return self._replace(distance=new_dist)
+
+
+class MatchedStop(NamedTuple):
+    stop_id: str
+    node_id: int
+    stop_sequence: int | None = None
+
+
+@dataclass
+class ForceViaPoint:
+    lat: float
+    lon: float
+    node_id: int = 0
+
+    def get_node_id(self, kd_tree: routx.KDTree) -> int:
+        if self.node_id == 0:
+            self.node_id = kd_tree.find_nearest_node(self.lat, self.lon).id
+        return self.node_id
+
+
+ShapeRequest: TypeAlias = Iterable[StopIdSequence]
+
+
+@dataclass
+class ShapeResponse:
+    points: list[LatLonDist] = field(default_factory=list[LatLonDist])
+    distances: dict[int, float] = field(default_factory=dict[int, float])
+
+
+@dataclass
+class LegRequest:
+    from_: MatchedStop
+    to: MatchedStop
+    max_distance_ratio: float = inf
+
+
+@dataclass
+class LegResponse:
+    from_: MatchedStop
+    to: MatchedStop
+    points: list[LatLonDist]
+
+    @classmethod
+    def prepare(cls, points: list[LatLonDist], request: LegRequest) -> Self:
+        return cls(request.from_, request.to, points)
